de6e8ed0bad00e9c1942e6ccd7fdb21a669d9991
cris <cristiriveropablos@gmail.com>
2023-02-02 21:29:32 +0100

    Tarea

--- src/main/java/org/springframework/samples/petclinic/vet/SpecialtyRepository.java
+++ src/main/java/org/springframework/samples/petclinic/vet/SpecialtyRepository.java
@@ -10,18 +10,8 @@ import jakarta.persistence.Cacheable;
 import jakarta.transaction.Transactional;
 
 public interface SpecialtyRepository extends Repository<Vet, Integer>{
-	@Transactional(readOnly = true)
-	@Cacheable("vets")
-	Collection<Vet> findAll() throws DataAccessException;
-
-	/**
-	 * Retrieve all <code>Vet</code>s from data store in Pages
-	 * @param pageable
-	 * @return
-	 * @throws DataAccessException
-	 */
-	@Transactional(readOnly = true)
-	@Cacheable("vets")
+	
+	
 	Page<Vet> findAll(Pageable pageable) throws DataAccessException;
 	
 }
